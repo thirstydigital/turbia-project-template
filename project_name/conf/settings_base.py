53a4733c94650ac5ad8bf46d933286d246ff5e74
Addy Yeow <addy.yeow@3030.com.au>
2013-12-17 16:53:32 +1100

    Incorporate turbia-demo into project template.

--- project_name/conf/settings_base.py
+++ project_name/conf/settings_base.py
@@ -81,6 +81,41 @@ COOOEEE = {
 	},
 }
 
+### DEMO SETTINGS #############################################################
+
+def CALLBACK(request):
+	"""
+	Returns True when conditions for skipping demo check are met.
+	"""
+	if request.user.is_superuser:
+		return True
+
+	exts = [
+		'css',
+		'gif',
+		'ico',
+		'jpg',
+		'js',
+		'png',
+	]
+	ext = request.path.split('.')[-1]
+	if ext in exts:
+		return True
+
+	apps = ['admin']
+	urls = ['demo_login', 'turbia_login']
+	try:
+		resolver_match = resolve(request.path)
+	except Http404:
+		return False
+	return (resolver_match.app_name in apps or resolver_match.url_name in urls)
+
+DEMO = {
+	'ENABLE': False,
+	'TOKEN': '_turbia_demo_token',
+	'CALLBACK': CALLBACK,
+}
+
 ### DJANGO SETTINGS ###########################################################
 
 ADMINS = [
@@ -131,6 +166,7 @@ INSTALLED_APPS = [
 #	'coooeee',
 #	'cron',
 #	'data',
+#	'demo',
 #	'dukebox',
 #	'fulfilment',
 #	'genericforms',
@@ -182,6 +218,8 @@ MIDDLEWARE_CLASSES = [
 #	'turbia.middleware.SessionQueryMiddleware',
 	'turbia.middleware.SiteMiddleware',
 #	'turbia.middleware.SiteRedirectMiddleware',
+
+#	'demo.middleware.DemoMiddleware',
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
